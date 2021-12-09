4e64faad845984c521688cd818e93cdd6b95d0d0
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2021-12-09 10:44:32 +0700

    set up project with express generator

---
+++ app/category/controller.js
@@ -0,0 +1,9 @@
+const index = async (req, res) => {
+    try {
+        res.render('index', { title: 'Category' });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = index
